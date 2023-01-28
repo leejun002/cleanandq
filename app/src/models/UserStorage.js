62a9b46166c0ff07459369048b638191eec41976
jyoungl <118170196+leejun002@users.noreply.github.com>
2023-01-28 04:57:47 +0000

    Implementation of membership routing

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -28,6 +28,14 @@ class UserStorage {
 
     return userInfo;
   }
+
+  static save(userInfo) {
+    const users = this.#users;
+    users.id.push(userInfo.id);
+    users.email.push(userInfo.email);
+    users.psword.push(userInfo.psword);
+    return { success: true };
+  }
 }
 
 module.exports = UserStorage;
